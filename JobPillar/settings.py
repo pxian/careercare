74ae5afbbf4d677173c1c87a953d30f31e110dd0
pxian <phooixian@hotmail.com>
2021-01-16 20:40:02 +0800

    Changes

--- JobPillar/settings.py
+++ JobPillar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nrqoko9rcv*m#c%t++@ef)i86+l)(y_)6e-m4hoj$#165^nn(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['careercare.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn JobPillar.wsgi --log-file -
